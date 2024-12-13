c6808d934b0bc199b5ec1f3e55f0f2c1851d6fbb
Eymen Hüda Aksu <eymenhuda0@gmail.com>
2024-12-13 17:30:36 +0300

    GM9PRO_sprout: Inherit from legacy QTI FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,6 +60,7 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/manifests/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifests/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 # Kernel
