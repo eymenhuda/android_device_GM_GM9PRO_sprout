450b8bb383d6d820887f4db6b57d41ae0ae7b747
yigityanik <yeyanik123@gmail.com>
2026-05-24 04:44:03 +0300

    GM9PRO_sprout: update display flags, add HDR and DCI-P3 wide color gamut support
    Actually, the GM 9 Pro's original screen supports HDR and wide color gamut. Official manufacturer data clearly states that Dolby Vision is supported on the screen (essentially an HDR feature).
    However, retail units doesn't support HDR features in the stock firmware.
    
    Before adding the Dolby Vision libraries, which work well on Android 11 and the GM 9 Pro, i included HDR support for the screen in the device tree.
    In addition, a custom screen calibration file was created for DCI P3 support. Wide color gamut flags was included in the device tree.
    
    Tested on two different devices. According to the test results, HDR support is partially available on the GM 9 Pro.
    Wide color gamut support is working. Compared to tests on the original screen, color tones have improved significantly (compared to stock firmware).
    
    Lastly, ideal DPI size and EGL flags were added to BoardConfig.mk
    
    --Yigit Emre YANIK (yigityanik)--

--- device.mk
+++ device.mk
@@ -48,6 +48,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Display Calibration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qdcm_calib_data_ams601nn13_amoled_fhd_cmd_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_ams601nn13_amoled_fhd_cmd_mode_dsi_panel.xml
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
