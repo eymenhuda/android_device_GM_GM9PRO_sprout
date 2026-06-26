de82f85cebff2c18d9e09ed93ff0255758ca79ee
yigityanik <yeyanik123@gmail.com>
2026-06-26 04:14:10 +0300

    GM9PRO_sprout: Switch to PixelExperience

--- device.mk
+++ device.mk
@@ -141,10 +141,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
 
-# DeviceDoze
-PRODUCT_PACKAGES += \
-    DeviceDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
