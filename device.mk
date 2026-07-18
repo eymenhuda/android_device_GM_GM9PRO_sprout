08e66154187124bfe6da1b83e0e990366ad374d4
Eymen Hüda Aksu <eymenhuda0@gmail.com>
2026-07-18 10:49:03 +0300

    GM9PRO_sprout: Switch to QTI AIDL vibrator implementation

--- device.mk
+++ device.mk
@@ -402,11 +402,6 @@ PRODUCT_HOST_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service \
-    android.hardware.vibrator@1.0-impl
-
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
