bc74b0fb4d1f14ec6bf0043582722234798f2ac7
Eymen Hüda Aksu <eymenhuda0@gmail.com>
2026-07-18 13:23:33 +0300

    GM9PRO_sprout: Enable Wi-Fi driver state control

--- device.mk
+++ device.mk
@@ -403,6 +403,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
 # WiFi
+WIFI_DRIVER_STATE_CTRL_PARAM := /dev/wlan
+WIFI_DRIVER_STATE_ON := ON
+WIFI_DRIVER_STATE_OFF := OFF
+
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
