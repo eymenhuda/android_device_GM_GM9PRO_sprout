dcb938e731cd24574210c87feef1a69c931aaf8f
yigityanik <yeyanik123@gmail.com>
2026-07-26 17:44:22 +0300

    GM9PRO_sprout: Build vendor variant of sensorservice and libpower

--- device.mk
+++ device.mk
@@ -378,8 +378,10 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-service \
     android.hardware.sensors@1.0-impl \
+    libpower.vendor \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
