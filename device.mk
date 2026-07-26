37f88e91e69891f7ebe801acd2b22c64a240b23e
yigityanik <yeyanik123@gmail.com>
2026-07-26 17:43:08 +0300

    GM9PRO_sprout: Build protobuf

--- device.mk
+++ device.mk
@@ -312,6 +312,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-vendorcompat
+
 # Postprocessing
 PRODUCT_PACKAGES += \
     libtinyxml
