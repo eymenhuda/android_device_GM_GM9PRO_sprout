4ed4473e5ac8556dec15865c9d6dc7040707cee5
Eymen Hüda Aksu <eymenhuda0@gmail.com>
2026-07-26 22:50:52 +0300

    GM9PRO_sprout: initialize legacy Wi-Fi driver

--- device.mk
+++ device.mk
@@ -417,11 +417,6 @@ PRODUCT_HOST_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
-# WiFi
-WIFI_DRIVER_STATE_CTRL_PARAM := /dev/wlan
-WIFI_DRIVER_STATE_ON := ON
-WIFI_DRIVER_STATE_OFF := OFF
-
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
