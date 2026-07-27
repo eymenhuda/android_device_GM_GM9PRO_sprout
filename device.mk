8cf0612a0aff154425d1cfae388c8396ba2d19a6
yigityanik <yeyanik123@gmail.com>
2026-07-27 11:19:40 +0300

    GM9PRO_sprout: Switch to AIDL vibrator hal
    I love yigityanik so much

--- device.mk
+++ device.mk
@@ -417,6 +417,11 @@ PRODUCT_HOST_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+   android.hardware.vibrator-service.legacy
+
+# WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
