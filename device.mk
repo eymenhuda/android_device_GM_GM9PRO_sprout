b06a883965f0a4294c62971e4fbbd775d3e78ad3
yigityanik <yeyanik123@gmail.com>
2026-07-27 10:33:04 +0300

    GM9PRO_sprout: Switch to QTI Memtrack AIDL HAL

--- device.mk
+++ device.mk
@@ -118,11 +118,9 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     vendor.qti.hardware.display.allocator-service \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    vendor.qti.hardware.memtrack-service \
     gralloc.sdm660 \
     hwcomposer.qcom \
-    memtrack.sdm660
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
