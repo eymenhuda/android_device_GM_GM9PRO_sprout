f380e9b7b11f3986985b7e164e27b358f829c4f5
Eymen Hüda Aksu <eymenhuda0@gmail.com>
2026-07-27 10:53:49 +0300

    GM9PRO_sprout: Build legacy health stuff
    Thanks for yigityanik

--- device.mk
+++ device.mk
@@ -176,7 +176,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery \
+    android.hardware.health@2.1-impl:64
 
 # HIDL
 PRODUCT_PACKAGES += \
