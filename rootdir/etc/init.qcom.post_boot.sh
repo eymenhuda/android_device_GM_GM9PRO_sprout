20073578c36f52e8d22de9347d53ff04472bcb53
Eymen Hüda Aksu <eymenhuda0@gmail.com>
2026-07-29 17:52:51 +0000

    GM9PRO_sprout: Tune SDM660 CPU and bandwidth configuration

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -2178,38 +2178,20 @@ case "$target" in
             # online CPU0
             echo 1 > /sys/devices/system/cpu/cpu0/online
             # configure governor settings for little cluster
-            echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-            echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load
-            echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif
-            echo "19000 1401600:39000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay
-            echo 82 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
-            echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
-            echo 1401600 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
-            echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy
-            echo "80 1401600:85 1747200:70" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
-            echo 30000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time
-            echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis
+            echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+            echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/rate_limit_us
+            echo 82 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_load
+            echo 1401600 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
             echo 902400 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-            echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif
-            echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/fast_ramp_down
 
             # online CPU4
             echo 1 > /sys/devices/system/cpu/cpu4/online
             # configure governor settings for big cluster
-            echo "interactive" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-            echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load
-            echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif
-            echo "19000 1958400:39000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay
-            echo 80 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
-            echo 20000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
-            echo 1958400 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
-            echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy
-            echo "78 1958400:85 2150400:68" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads
-            echo 30000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time
-            echo 39000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis
+            echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+            echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/rate_limit_us
+            echo 80 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_load
+            echo 1958400 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_freq
             echo 1401600 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
-            echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/ignore_hispeed_on_notif
-            echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/fast_ramp_down
 
             # bring all cores online
             echo 1 > /sys/devices/system/cpu/cpu0/online
@@ -2252,7 +2234,7 @@ case "$target" in
             configure_memory_parameters
 
             # Enable bus-dcvs
-            for cpubw in /sys/class/devfreq/*qcom,cpubw*
+            for cpubw in /sys/class/devfreq/soc:qcom,cpu-cpu-ddr-bw
             do
                 echo "bw_hwmon" > $cpubw/governor
                 echo 50 > $cpubw/polling_interval
