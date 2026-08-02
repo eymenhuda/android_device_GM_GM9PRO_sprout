830960cecc29aa501c8a114b611991a47fa2300c
Eymen Hüda Aksu <eymenhuda0@gmail.com>
2026-08-02 17:01:19 +0300

    GM9PRO_sprout: Start Power HAL hints after boot

--- device.mk
+++ device.mk
@@ -317,7 +317,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
+    $(LOCAL_PATH)/rootdir/etc/init.gm9pro-power.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.gm9pro-power.rc
 
 # Privapp Permissions
 PRODUCT_COPY_FILES += \
